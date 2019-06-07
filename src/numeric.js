8ac076cfa473cc250c79a0f80e976388884c1389
Tom MacWright <tom@macwright.org>
2019-06-07 11:11:57 -0700

    Build setup for the web

--- src/numeric.js
+++ src/numeric.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var numeric = (typeof exports === "undefined")?(function numeric() {}):(exports);
-if(typeof global !== "undefined") { global.numeric = numeric; }
+export const numeric = {};
 
 numeric.version = "1.2.6";
 
